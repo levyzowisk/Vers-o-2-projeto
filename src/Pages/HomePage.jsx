60f357993a82d19df501fa72808ec6028b4cb926
Dirceusljr <dirceusljr@gmail.com>
2024-07-18 15:32:49 -0300

    Componente Header Terminado

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
+import Header from '../Components/Header/Header'
 import Footer from '../Components/Footer/Footer'
 function HomePage() {
 
   return (
     <>
-
+      <Header></Header>
       <Footer></Footer>
-
     </>
   )
 }
